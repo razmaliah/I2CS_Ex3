9610fc60ad1bdbe059883e0158b180c55982aed9
raz maliah <razml9436@gmail.com>
2026-01-18 14:43:27 +0200

    with best results (2 dots left, level 4 dt 200), need to organize the code and to finish testing

--- src/Ex3Algo.java
+++ src/Ex3Algo.java
@@ -22,7 +22,7 @@ public class Ex3Algo implements PacManAlgo {
     public static Index2D _pacPos = null;
     public static Pixel2D[] _ghostsPos = null;
     public static boolean _isEatable = false;
-    private static final int CLOSE_GHOST_DIST = 8;
+    private static final int MAX_CLOSE_GHOST_DIST = 8;
     public static Pixel2D[] _greenDots = {new Index2D(3,5), new Index2D(3,18), new Index2D(19,5), new Index2D(19,18)};
 
     public Ex3Algo() {
@@ -63,13 +63,13 @@ public class Ex3Algo implements PacManAlgo {
         updateGhosts();
         updateMap();
 
-        int distt = distToClosestGhost(ghostDist());
-        if(distt < CLOSE_GHOST_DIST){
+        int distTCG = distToClosestGhost(ghostDist());
+        if(distTCG < MAX_CLOSE_GHOST_DIST){
             if(_isEatable){
                return huntClosestGhostDir();
             }
             else{
-               if(distToClosestGreenDot() < distToClosestGhost(ghostDist())){
+               if(distToClosestGreenDot() < distTCG){
                    return dirToClosestGreenDot();
                }
                else{
@@ -109,7 +109,33 @@ public class Ex3Algo implements PacManAlgo {
         return dirs[ind];
     }
 
-    ///  ///////////////////////////////////// My private methods  /////////////////////////////////////
+    ///  ///////////////////////////////////// My methods  /////////////////////////////////////
+
+    public static void updatePac() {
+        String pos = _game.getPos(1);
+        int x = Integer.parseInt(pos.split(",")[0]);
+        int y = Integer.parseInt(pos.split(",")[1]);
+        Index2D p1 = new Index2D(x, y);
+        _pacPos = p1;
+    }
+
+    public static void updateGhosts() {
+        GhostCL[] ghosts = _game.getGhosts(0);
+        Pixel2D[] ghostPositions = new Pixel2D[ghosts.length];
+        for (int i = 0; i < ghosts.length; i++) {
+            ghostPositions[i] = ghostPos(ghosts[i]);
+        }
+        _ghostsPos = ghostPositions;
+        double eat = _game.getGhosts(1)[0].remainTimeAsEatable(0);
+        if(eat > 0){_isEatable = true;}
+        else{_isEatable = false;}
+    }
+
+    public static void updateMap(){
+        if (_game == null){ return; }
+        int[][] arr2D = _game.getGame(0);
+        _map = new Map(arr2D);
+    }
 
     public static Pixel2D[] closestPP() {
         Pixel2D[] ans = null;
@@ -118,15 +144,6 @@ public class Ex3Algo implements PacManAlgo {
         Pixel2D target = closestFood();
         ans = _map.shortestPath(_pacPos, target, OBS_VALUE);
         return ans;
-    }
-
-
-    public static void updatePac() {
-        String pos = _game.getPos(1);
-        int x = Integer.parseInt(pos.split(",")[0]);
-        int y = Integer.parseInt(pos.split(",")[1]);
-        Index2D p1 = new Index2D(x, y);
-        _pacPos = p1;
     }
 
     public static Index2D closestFood() {
@@ -152,18 +169,6 @@ public class Ex3Algo implements PacManAlgo {
         if(next.getY() == start.getY() + 1 || start.getY() - next.getY() > 1){return Game.UP;}
         if(next.getY() + 1 == start.getY() || next.getY() - start.getY() > 1){return Game.DOWN;}
         return Game.UP;
-    }
-
-    public static void updateGhosts() {
-        GhostCL[] ghosts = _game.getGhosts(0);
-        Pixel2D[] ghostPositions = new Pixel2D[ghosts.length];
-        for (int i = 0; i < ghosts.length; i++) {
-            ghostPositions[i] = ghostPos(ghosts[i]);
-        }
-        _ghostsPos = ghostPositions;
-        double eat = _game.getGhosts(1)[0].remainTimeAsEatable(0);
-        if(eat > 0){_isEatable = true;}
-        else{_isEatable = false;}
     }
 
     public static Index2D ghostPos (GhostCL g){
@@ -259,93 +264,6 @@ public class Ex3Algo implements PacManAlgo {
         Pixel2D[] SP = _map.shortestPath(start, target, OBS_VALUE);
         target = SP[1];
         return getDir(start,target);
-    }
-
-    public static int distBetween(Pixel2D p1, Pixel2D p2){
-        Map2D allD = _map.allDistance(p1, OBS_VALUE);
-        return allD.getPixel(p2);
-    }
-
-    public static void updateMap(PacmanGame game){
-        if (_game == null){ return; }
-        int[][] arr2D = game.getGame(0);
-        _map = new Map(arr2D);
-    }
-
-    public static void updateMap(){
-        updateMap(_game);
-    }
-
-
-
-    public static int run() {
-        updatePac();
-        updateGhosts();
-        updateMap();
-        int ind = indexClosestGhost(ghostDist());
-        Pixel2D target = _ghostsPos[ind];
-        Pixel2D start = _pacPos;
-        Pixel2D[] SP = _map.shortestPath(start, target, OBS_VALUE);
-        target = SP[1];
-        int dirGhost = getDir(start, target);
-        if (dirGhost == Game.UP) {
-            return runToDown();
-        }
-        if (dirGhost == Game.LEFT) {
-            return runToRight();
-        }
-        if (dirGhost == Game.DOWN) {
-            return runToUp();
-        }
-        if( dirGhost == Game.RIGHT) {
-            return runToLeft();
-        }
-        return randomDir();
-    }
-
-
-    private static int runToRight () {
-        updatePac();
-        updateMap();
-        Pixel2D start = _pacPos;
-        Pixel2D target = new Index2D((start.getX() + 1), start.getY());
-        if (_map.getPixel(target) == OBS_VALUE) {
-            return runToUp();
-        }
-        return Game.RIGHT;
-    }
-
-    private static int runToUp () {
-        updatePac();
-        updateMap();
-        Pixel2D start = _pacPos;
-        Pixel2D target = new Index2D(start.getX(), (start.getY() + 1));
-        if (_map.getPixel(target) == OBS_VALUE) {
-            return runToLeft();
-        }
-        return Game.UP;
-    }
-
-    private static int runToLeft () {
-        updatePac();
-        updateMap();
-        Pixel2D start = _pacPos;
-        Pixel2D target = new Index2D((start.getX() - 1), start.getY());
-        if (_map.getPixel(target) == OBS_VALUE) {
-            return runToDown();
-        }
-        return Game.LEFT;
-    }
-
-    private static int runToDown () {
-        updatePac();
-        updateMap();
-        Pixel2D start = _pacPos;
-        Pixel2D target = new Index2D(start.getX(), (start.getY() - 1));
-        if (_map.getPixel(target) == OBS_VALUE) {
-            return runToRight();
-        }
-        return Game.DOWN;
     }
 
     public static Map2D combainedGhostDistMaps() {
